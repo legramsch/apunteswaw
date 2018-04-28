8bb5cbcb31126dc1afa611d791ea8dbd276dde6e
legramsch <legramsch@uc.cl>
2018-04-28 19:13:42 -0300

    asd

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from socket import *
 import flask
-s = socket(AF_INET, SOCK_DGRAM)
-s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+#s = socket(AF_INET, SOCK_DGRAM)
+#s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 
 app = flask.Flask(__name__)
 @app.route('/apunteswaw', methods=['POST'])
 def respuesta():
 	data = flask.request.data
-	s.sendto(b'1', ('127.0.0.1', 8090))
+#	s.sendto(b'1', ('127.0.0.1', 8090))
 	print('Enviado')
 	return '<h1>Titulo</h1>'
 @app.route('/mandaimg', methods=['GET'])
